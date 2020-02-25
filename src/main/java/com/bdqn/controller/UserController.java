ac1f8052ffc06e50c8a2b35ed7fb2b0b646bf30c
WangJiangYi-git <2188771989@qq.com>
2020-02-25 11:48:49 +0800

    test03

--- src/main/java/com/bdqn/controller/UserController.java
+++ src/main/java/com/bdqn/controller/UserController.java
@@ -1,9 +1,15 @@
 package com.bdqn.controller;
 
+import com.bdqn.response.Result;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
+    @GetMapping("/add")
+    public Result add(){
+        return null;
+    }
 }
